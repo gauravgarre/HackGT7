c1aff4d2405061699e58a3a3f27b05fd469e1d69
Gaurav Garre <65045499+gauravgarre@users.noreply.github.com>
2020-10-17 15:09:54 -0400

    flask error naming

--- flaskr/hello.py
+++ flaskr/hello.py
@@ -28,9 +28,10 @@ def create():
         password = request.form['password']
         email = request.form['email']
         birthDate = request.form['birthDate']
-        #regex_email = '[^@]+@[^@]+\.[^@]+'
+        # regex_email = '[^@]+@[^@]+\.[^@]+'
         regex_bday = '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]'
-        if not (20 > len(firstName) > 0 and 20 > len(lastName) > 0 and 20 > len(username) > 0 and 20 > len(password) > 6):
+        if not (20 > len(firstName) > 0 and 20 > len(lastName) > 0 and 20 > len(username) > 0 and 20 > len(
+                password) > 6):
             return "Record not found", 400
         if not re.search(regex_bday, birthDate):
             return "Record not found", 400
@@ -40,59 +41,67 @@ def create():
         data = [firstName, lastName, username, password, email, birthDate]
         print(data)
         try:
-            cursor.execute("INSERT INTO users (firstName, lastName ,username, password, email, birthDate) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}')".format(
-            data[0], data[1], data[2], data[3], data[4], data[5]))
+            cursor.execute(
+                "INSERT INTO users (firstName, lastName ,username, password, email, birthDate) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}')".format(
+                    data[0], data[1], data[2], data[3], data[4], data[5]))
         except mysql.connector.Error:
-            return ("User with username already exists",400)
-        cursor.execute("CREATE TABLE events{0} (name varchar(255), expectedTime int , startDateTime DATETIME, repeatTime varchar(255), numTimesMissed int)".format(data[2]))
+            return ("User with username already exists", 400)
+        cursor.execute(
+            "CREATE TABLE events{0} (name varchar(255), expectedTime int , startDateTime DATETIME, repeatTime varchar(255), numTimesMissed int)".format(
+                data[2]))
         cnx.commit()
         return "Success", 200
-        
-@app.route('/delete',methods=['POST'])
+
+
+@app.route('/delete', methods=['POST'])
 def delete():
     if request.method == 'POST':
-        username, password = request.form['username'],request.form['password']
+        username, password = request.form['username'], request.form['password']
         cursor.execute("DELETE FROM users WHERE username = '{0}' AND password = '{1}'".format(username, password))
         cnx.commit()
         return "Success", 200
+
 
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
         username, password = request.form['username'], request.form['password']
         if 20 < len(username) < 6 and 20 < len(password) < 6:
-            return "Record not found", 400
-        #cursor.execute("")
-        #cnx.commit()
+            return "Username and/or password invalid length", 400
+        # cursor.execute("")
+        # cnx.commit()
         return "Success", 200
 
-@app.route('/event/add',methods=['POST'])
+
+@app.route('/event/add', methods=['POST'])
 def addEvent():
     if request.method == 'POST':
         name, expectedTime, startDateTime, repeat, numTimesMissed = request.form['name'], request.form['expectedTime'], \
-                                                                   request.form['startDateTime'], request.form['repeat'], \
-                                                                   request.form['numTimesMissed']
+                                                                    request.form['startDateTime'], request.form[
+                                                                        'repeat'], \
+                                                                    request.form['numTimesMissed']
 
         regex_startDateTime = '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]'
         if not 0 < len(name) < 20:
-            return "Record not found", 400
+            return "Name invalid length", 400
         elif not re.search(regex_startDateTime, startDateTime):
-            return "Record not found", 400
-        elif not repeat in ('daily', 'weekly', 'monthly', 'none'):
-            return "Record not found", 400
+            return "startDateTime invalid format", 400
+        elif not (repeat in ('daily', 'weekly', 'monthly', 'none')):
+            return "repeat invalid format", 400
 
         event_data = [name, expectedTime, startDateTime, repeat, numTimesMissed]
         return "Success", 200
 
+
 @app.route('/event/get', methods=['POST'])
 def getEvents():
-     if request.method == 'POST':
-         username, checkPassword = request.form['username'],request.form['password']
-         cursor.execute("SELECT * FROM users WHERE username='{0}'".format(username))
-         for password in cursor:
-             if (password == password):
-                 cursor.execute("SELECT * FROM events{0}".format(username))
-                 for name, expectedTime, startDateTime, repeat, numTimesMissed in cursor:
-                     print(name, expectedTime, startDateTime, repeat, numTimesMissed)
-                 #execute
-                 return "sample"
+    if request.method == 'POST':
+        username, checkPassword = request.form['username'], request.form['password']
+        cursor.execute("SELECT * FROM users WHERE username='{0}'".format(username))
+        for password in cursor:
+            if (password == password):
+                cursor.execute("SELECT * FROM events{0}".format(username))
+                for name, expectedTime, startDateTime, repeat, numTimesMissed in cursor:
+                    print(name, expectedTime, startDateTime, repeat, numTimesMissed)
+                # execute
+                return "sample"
