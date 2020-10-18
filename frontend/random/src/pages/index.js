33d6ce56cba4d68ca8e3d7ef2173e98bd36c8d80
sranganath02 <71161980+sranganath02@users.noreply.github.com>
2020-10-18 01:15:38 -0400

    Almost everything

---
+++ frontend/random/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Mainpage = () =>{
+  return(
+  <div>
+  <h3>Welcome to the React Router </h3>
+  <small>Main Page</small>
+  </div>
+  );
+};
+export default Mainpage;
