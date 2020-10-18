b17d0fde24a1ba8baac6f7d4c27068aff84de7c4
sranganath02 <71161980+sranganath02@users.noreply.github.com>
2020-10-18 04:21:35 -0400

    Update login.component.js

--- frontend/random/src/react-login-signup-ui-template/src/login.component.js
+++ frontend/random/src/react-login-signup-ui-template/src/login.component.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+
+  constructor (props){
+    super(props)
+    this.state ={
+      uName:"",
+      pass:""
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event){
+    const name = event.target.name;
+    this.setState({
+      [name]: event.target.value
+    })
+  }
+
+
   render() {
       return (
           <form>
@@ -8,15 +25,15 @@ export default class Login extends Component {
 
               <div className="form-group">
                   <label>Username</label>
-                  <input type="text" className="form-control" placeholder="Enter username" />
+                  <input name = "uName" value = {this.state.uName} onChange = {this.handleChange} type="text" className="form-control" placeholder="Enter username" />
               </div>
 
               <div className="form-group">
                   <label>Password</label>
-                  <input type="password" className="form-control" placeholder="Enter password" />
+                  <input name = "pass" value = {this.state.pass} onChange = {this.handleChange} type="password" className="form-control" placeholder="Enter password" />
               </div>
 
-              <button type="submit" onClick = {console.log("Hello")}className="btn btn-primary btn-block">Submit</button>
+              <button type="submit" className="btn btn-primary btn-block">Submit</button>
 
           </form>
       );
