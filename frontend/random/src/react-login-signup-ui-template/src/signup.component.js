726edad13ec8f780e88e5ece9e784addc68574a4
sranganath02 <71161980+sranganath02@users.noreply.github.com>
2020-10-18 03:26:41 -0400

    Update signup.component.js

--- frontend/random/src/react-login-signup-ui-template/src/signup.component.js
+++ frontend/random/src/react-login-signup-ui-template/src/signup.component.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
-import Axios from "Axios";
+var axios = require("axios");
 
-export default class SignUp extends Component {
+
+class SignUp extends Component {
   constructor (props){
     super(props)
     this.state ={
-
+      fName:"",
+      lName:"",
+      uName:"",
+      pass:"",
+      email:"",
+      bday:"1000-01-01"
     }
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange(event){
+    const name = event.target.name;
+    this.setState({
+      [name]: event.target.value
+    })
+  }
+
+  create(){
+    axios.post('http://127.0.0.1:5000/create', {firstName:'this.state.fName',lastName:'this.state.lName', username:'this.state.uName',password:'this.state.pass',email:'this.state.email',birthDate:'this.state.bday'}).then(
+      function (response){
+          console.log(response.data)
+      }
+  ).catch((e)=>{
+      console.log(e)
+  })
+}
+
   render() {
       return (
           <form>
@@ -15,25 +39,29 @@ export default class SignUp extends Component {
 
               <div className="form-group">
                   <label>First name</label>
-                  <input type="text" className="form-control" placeholder="First name" />
+                  <input type="text" name = "fName" value = {this.state.fName} onChange = {this.handleChange} className="form-control" placeholder="First name" />
               </div>
 
               <div className="form-group">
                   <label>Last name</label>
-                  <input type="text" className="form-control" placeholder="Last name" />
+                  <input type="text" name = "lName" value = {this.state.lName} onChange = {this.handleChange} className="form-control" placeholder="Last name" />
               </div>
 
               <div className="form-group">
                   <label>Username</label>
-                  <input type="text" className="form-control" placeholder="Enter username" />
+                  <input type="text" name = "uName" value = {this.state.uName} onChange = {this.handleChange} className="form-control" placeholder="Username" />
               </div>
 
               <div className="form-group">
                   <label>Password</label>
-                  <input type="password" className="form-control" placeholder="Enter password" />
+                  <input type="password" name = "pass" value = {this.state.pass}  onChange = {this.handleChange} className="form-control" placeholder="Password" />
+              </div>
+              <div className="form-group">
+                  <label>Email</label>
+                  <input type="Email" name = "email" value = {this.state.email}  onChange = {this.handleChange} className="form-control" placeholder="Email" />
               </div>
 
-              <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+              <button type="submit" onClick = {this.create} className="btn btn-primary btn-block">Sign Up</button>
               <p className="forgot-password text-right">
                   Already registered <a href="http://localhost:3000/sign-in">sign in?</a>
               </p>
@@ -41,3 +69,4 @@ export default class SignUp extends Component {
       );
   }
 }
+export default SignUp;
